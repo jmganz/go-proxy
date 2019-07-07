c6f41b30b2242a63a10e5d3bea17abfc58011b68
Jonathan <jganz@cloudflare.com>
2019-07-07 15:41:22 -0700

    Identify Simple Proxy Protocol packets based on the magic number 0x56EC (Will this magic number always appear at the same offset?)

--- go-proxy.go
+++ go-proxy.go
@@ -2,6 +2,7 @@ package main
 
 import "log"
 //import "net"
+import "encoding/binary"
 
 //import "github.com/docopt/docopt-go"
 //import "github.com/ghedo/go.pkt/routing"
@@ -41,21 +42,54 @@ func main() {
       log.Fatal(err)
     }
 
-    //log.Println(pkt)
-    //log.Println("")
-    //log.Println(pkt.Payload().Payload().GetType())
-    payload := pkt.Payload()
-    if (nil != payload) {
-      payload = payload.Payload()
-      if (nil != payload) {
-        packetType := payload.GetType()
-        if ("UDP" == packetType.String()) {
-          log.Println(packetType)
-          log.Println(payload.GetLength())
-          log.Println("")
+    //log.Println(buf)
+
+    udp_pkt := layers.FindLayer(pkt, packet.UDP)
+    if (nil != udp_pkt) {
+      if (len(buf) > 44) {
+        if ( 0x56EC == binary.BigEndian.Uint16(buf[42:44]) ) {
+          log.Println("Found a Proxy Packet!")
+          //srcIP := binary.BigEndian.Uint32(buf[56:60])
+          //dstIP := binary.BigEndian.Uint32(buf[72:76])
+          log.Printf("client IP: %d.%d.%d.%d\n", buf[56], buf[57], buf[58], buf[59])
+          log.Printf("proxy IP: %d.%d.%d.%d\n", buf[72], buf[73], buf[74], buf[75])
         }
       }
     }
+
+
+
+
+
+/*
+    data := layers.FindLayer(pkt, packet.Raw)
+    if (nil != data) {
+      //log.Println(udp_pkt)
+      //data := udp_pkt.Payload()
+      log.Println(data)
+    }
+*/
+    /*
+    payload := pkt.Payload() // IP
+    if (nil != payload) {
+      payload = payload.Payload() // UDP / TCP
+      if (nil != payload) {
+        packetType := payload.GetType()
+        if ("UDP" == packetType.String()) {
+          //log.Println(packetType)
+          udpData := payload.Payload()
+          if (nil != udpData) {
+            log.Println(buf)
+            log.Println(packet.UDP)
+            //unpacked := udpData.Unpack(newBuf)
+            //log.Println(udpData.GetLength())
+            //log.Println(unpacked)
+            log.Println("")
+          }
+        }
+      }
+    }
+    */
     /*
     if (pkt.Match("UDP")) {
       log.Println("UDP!")
