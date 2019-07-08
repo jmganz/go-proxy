2ddd63835aa231ba901ee935cb35b0f05e05ee46
Jonathan <jganz@cloudflare.com>
2019-07-07 23:19:22 -0700

    Working on rewriting the packet to remove the Simple Proxy Protocol header

--- go-proxy.go
+++ go-proxy.go
@@ -11,6 +11,7 @@ import "github.com/ghedo/go.pkt/capture/pcap"
 import "github.com/ghedo/go.pkt/layers"
 import "github.com/ghedo/go.pkt/packet"
 //import "github.com/ghedo/go.pkt/packet/udp"
+//import "github.com/ghedo/go.pkt/packet/ipv4"
 
 func main() {
   log.SetFlags(0)
@@ -42,17 +43,36 @@ func main() {
       log.Fatal(err)
     }
 
-    //log.Println(buf)
-
     udp_pkt := layers.FindLayer(pkt, packet.UDP)
     if (nil != udp_pkt) {
       if (len(buf) > 44) {
         if ( 0x56EC == binary.BigEndian.Uint16(buf[42:44]) ) {
+          //log.Println(pkt)
+          log.Println(buf)
+          log.Println("")
           log.Println("Found a Proxy Packet!")
           //srcIP := binary.BigEndian.Uint32(buf[56:60])
           //dstIP := binary.BigEndian.Uint32(buf[72:76])
-          log.Printf("client IP: %d.%d.%d.%d\n", buf[56], buf[57], buf[58], buf[59])
-          log.Printf("proxy IP: %d.%d.%d.%d\n", buf[72], buf[73], buf[74], buf[75])
+          log.Printf("client - %d.%d.%d.%d:%d\n", buf[56], buf[57], buf[58], buf[59], binary.BigEndian.Uint16(buf[76:78]))
+          log.Printf("proxy - %d.%d.%d.%d:%d\n", buf[72], buf[73], buf[74], buf[75], binary.BigEndian.Uint16(buf[78:80]))
+          log.Println(string(buf[80:]))
+          ip_pkt := layers.FindLayer(pkt, packet.IPv4)
+          log.Println(ip_pkt)
+          //log.Println(ip_pkt)
+          /*
+          packet := ipv4.Packet {
+            Version: 4,
+            IHL: 5,
+            Length: 28,
+            TOS: 80,
+            Id: 15,
+            TTL: 56,
+            Flags: ipv4.DontFragment,
+            Protocol: ipv4.UDP,
+            SrcAddr: buf[56:60],
+            DstAddr: buf[72:76],
+          }
+          */
         }
       }
     }
