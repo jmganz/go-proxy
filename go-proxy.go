b6d548e2498c400fe0016b7fc4c67897d68132bd
Jonathan <ganz@Phoenix>
2019-07-30 22:46:28 -0700

    Small tweaks - reduce the noise of my debug output; change the ports over which communication occurs

--- go-proxy.go
+++ go-proxy.go
@@ -51,12 +51,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(addrs)
+	//log.Println(addrs)
 
 	for _, a := range addrs {
 		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
-				log.Println(ipnet.IP.String())
+				//log.Println(ipnet.IP.String())
 			}
 		}
 	}
@@ -78,30 +78,30 @@ func main() {
       if (len(buf) > 44) {
         if ( 0x56EC == binary.BigEndian.Uint16(buf[42:44]) ) {
           //log.Println(pkt)
-          log.Println("START")
+          //log.Println("START")
 
-          log.Println(buf)
-          log.Println("ETHERNET")
-          log.Println(pkt)
-          log.Println("IPv4")
-          ip_pkt := layers.FindLayer(pkt, packet.IPv4)
-          log.Println(ip_pkt)
-          log.Println("UDP")
-          log.Println(udp_pkt)
-          log.Println("Found a Proxy Packet!")
+          //log.Println(buf)
+          //log.Println("ETHERNET")
+          //log.Println(pkt)
+          //log.Println("IPv4")
+          //ip_pkt := layers.FindLayer(pkt, packet.IPv4)
+          //log.Println(ip_pkt)
+          //log.Println("UDP")
+          //log.Println(udp_pkt)
+          log.Println("\n\n\n*** Found a Proxy Packet ***")
           log.Printf("client - %d.%d.%d.%d:%d\n", buf[56], buf[57], buf[58], buf[59], binary.BigEndian.Uint16(buf[76:78]))
           log.Printf("proxy - %d.%d.%d.%d:%d\n", buf[72], buf[73], buf[74], buf[75], binary.BigEndian.Uint16(buf[78:80]))
           log.Println(string(buf[80:]))
-          log.Println("FINISH")
+          log.Println("*** FINISH ***")
 
-          log.Println("*** new ether ***")
+          //log.Println("*** new ether ***")
           eth_pkt := eth.Make()
           eth_pkt.DstAddr = buf[0:6]
           eth_pkt.SrcAddr = buf[6:12]
           eth_pkt.Type = eth.IPv4
-          log.Println(eth_pkt)
+          //log.Println(eth_pkt)
 
-          log.Println("*** new ipv4 ***")
+          //log.Println("*** new ipv4 ***")
           ip4_pkt := ipv4.Make()
           ip4_pkt.Version = ((buf[14] >> 4) & 0xF)
           ip4_pkt.IHL = (buf[14] & 0xF)
@@ -116,25 +116,25 @@ func main() {
           ip4_pkt.TTL = uint8(buf[22])
           ip4_pkt.Protocol = ipv4.UDP
           //ip4_pkt.Checksum = 0x51a9 // TODO: compute this properly
-          log.Println("*** SOURCE ADDRESSES ***")
-          log.Println(buf[56:60])
+          //log.Println("*** SOURCE ADDRESSES ***")
+          //log.Println(buf[56:60])
           //ip4_pkt.SrcAddr = buf[56:60]
-          log.Println(buf[72:76])
+          //log.Println(buf[72:76])
           ip4_pkt.SrcAddr = buf[72:76]
           //ip4_pkt.DstAddr = buf[72:76]
           ip4_pkt.DstAddr = buf[30:34]
-          log.Println(ip4_pkt)
+          //log.Println(ip4_pkt)
 
-          log.Println("*** new udp ***")
+          //log.Println("*** new udp ***")
           fwd_udp := udp.Make()
           fwd_udp.SrcPort = binary.BigEndian.Uint16(buf[76:78])
           fwd_udp.DstPort = binary.BigEndian.Uint16(buf[78:80])
-          log.Println(fwd_udp)
+          //log.Println(fwd_udp)
 
-          log.Println("*** new data ***")
+          //log.Println("*** new data ***")
           raw_pkt := raw.Make()
           raw_pkt.Data = buf[80:]
-          log.Println(raw_pkt)
+          //log.Println(raw_pkt)
 
           fwd_udp.SetPayload(raw_pkt)
           ip4_pkt.SetPayload(fwd_udp)
@@ -144,11 +144,10 @@ func main() {
           if err != nil {
             log.Fatal("Error packing: %s", err)
           }
-          log.Println("HERE'S THE NEW BUFFER")
+          log.Println("New Buffer:")
           log.Println(buf)
 
-          log.Println("HERE'S THE NEW PACKET")
-
+          log.Println("New Packet:")
           new_pkt, err := layers.UnpackAll(buf, packet.Eth)
           if err != nil {
             log.Fatal(err)
@@ -163,7 +162,6 @@ func main() {
         } // packet contains magic number 0x56EC, so it's a Proxy Packet
       } // length > 44 bytes
       if ( len(buf) < 45 || 0x56EC != binary.BigEndian.Uint16(buf[42:44]) ) {
-        //var proxy_ports = []uint16 {4242, 4252, 7777, 7778, 7787, 7788}
         proxy_ports := map[uint16]bool {4242:true, 4252:true, 7777:true, 7778:true, 7787:true, 7788:true, 26900:true, 26901:true, 26902:true, 26903:true, 26904:true, 26905:true, 26910:true, 26911:true, 26912:true, 26913:true, 26914:true, 26915:true, 26916:true, 26917:true, 26918:true, 26919:true, 26920:true, 27025:true, 27026:true, 27027:true, 27028:true, 27029:true, 27030:true, 27215:true, 27225:true, 32330:true, 32340:true}
         origin_ip, err := strconv.Atoi(os.Getenv("ORIGINIP"))
         if err != nil {
@@ -182,20 +180,20 @@ func main() {
           // buf[34:36] is src port
           // buf[36:38] is dst port
 
-          log.Println("*** Convert Non-Proxy Packet ***")
-          log.Println(buf)
-          log.Println("ETHERNET")
-          log.Println(pkt)
-          log.Println("IPv4")
-          ip_pkt := layers.FindLayer(pkt, packet.IPv4)
-          log.Println(ip_pkt)
-          log.Println("UDP")
-          log.Println(udp_pkt)
+          log.Println("\n\n\n*** Convert Non-Proxy Packet ***")
+          //log.Println(buf)
+          //log.Println("ETHERNET")
+          //log.Println(pkt)
+          //log.Println("IPv4")
+          //ip_pkt := layers.FindLayer(pkt, packet.IPv4)
+          //log.Println(ip_pkt)
+          //log.Println("UDP")
+          //log.Println(udp_pkt)
           //log.Println(string(buf[80:]))
           log.Println("*** data ***")
           raw_pkt := raw.Make()
           raw_pkt.Data = buf[42:]
-          log.Println(raw_pkt.Data)
+          //log.Println(raw_pkt.Data)
           log.Println(string(buf[42:len(buf)]))
           log.Println("packet is ", len(buf), " bytes long")
           log.Println("*** FINISH ***")
@@ -230,7 +228,9 @@ func main() {
           //buf[76:78] = binary.BigEndian.Uint16(udp_pkt.SrcPort)
           binary.BigEndian.PutUint16(buf2[76:78], binary.BigEndian.Uint16(buf[34:36]))
           //buf[78:80] = binary.BigEndian.Uint16(udp_pkt.DstPort)
-          binary.BigEndian.PutUint16(buf2[78:80], binary.BigEndian.Uint16(buf[36:38]))
+          // binary.BigEndian.PutUint16(buf2[78:80], binary.BigEndian.Uint16(buf[36:38]))
+          // Do I need to add 10 to the port number to handle the proxy port communication?
+          binary.BigEndian.PutUint16(buf2[78:80], binary.BigEndian.Uint16(buf[36:38]) + 10)
           log.Printf("client - %d.%d.%d.%d:%d\n", buf2[56], buf2[57], buf2[58], buf2[59], binary.BigEndian.Uint16(buf2[76:78]))
           log.Printf("proxy - %d.%d.%d.%d:%d\n", buf2[72], buf2[73], buf2[74], buf2[75], binary.BigEndian.Uint16(buf2[78:80]))
           //ip4_pkt.SrcAddr = buf[6:12]
@@ -253,15 +253,15 @@ func main() {
           ip4_pkt.SetPayload(fwd_udp)
           eth_pkt.SetPayload(ip4_pkt)
 
-          buf, err := layers.Pack(eth_pkt, ip4_pkt, fwd_udp, raw_pkt)
+          buf2, err := layers.Pack(eth_pkt, ip4_pkt, fwd_udp, raw_pkt)
           if err != nil {
             log.Fatal("Error packing: %s", err)
           }
-          log.Println("HERE'S THE NEW BUFFER")
-          log.Println(buf)
 
-          log.Println("HERE'S THE NEW PACKET")
+          log.Println("New Buffer:")
+          log.Println(buf2)
 
+          log.Println("New Packet:")
           new_pkt, err := layers.UnpackAll(buf, packet.Eth)
           if err != nil {
             log.Fatal(err)
